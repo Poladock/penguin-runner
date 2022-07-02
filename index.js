ce56c99934fee11dec218dbdafefa7770a94036c
ppolakit <polakritlogic@gmail.com>
2022-07-02 22:39:30 +0700

    p

--- index.js
+++ index.js
@@ -45,7 +45,7 @@ pool.connect((err, client, release) => {
 	})
 })
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
 	console.log("TEST DATA :");
 	pool.query('Select * from test')
 		.then(testData => {
